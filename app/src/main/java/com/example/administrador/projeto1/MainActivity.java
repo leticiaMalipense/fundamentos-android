1da2b5e032def3f5579477c5a83627844f8fb0a7
Leticia <leticia.malipense@gmail.com>
2015-07-20 17:56:38 -0300

    Meu primeiro comite.

---
+++ app/src/main/java/com/example/administrador/projeto1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.administrador.projeto1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+
+        TextView minhaView = (TextView) findViewById(R.id.textView);
+        minhaView.setText("Testando....");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Cliquei!!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}

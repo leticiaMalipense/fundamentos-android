f599a35ea57de8295f51a866a6e2edcddb2b7e7e
Leticia <leticia.malipense@gmail.com>
2015-07-27 21:45:34 -0300

    Finalização da aula 27/07.

--- app/src/main/java/com/example/administrador/projeto1/controller/SaveClientActivity.java
+++ app/src/main/java/com/example/administrador/projeto1/controller/SaveClientActivity.java
@@ -1,16 +1,19 @@
 package com.example.administrador.projeto1.controller;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.administrador.projeto1.R;
 import com.example.administrador.projeto1.model.entities.Client;
+import com.example.administrador.projeto1.model.entities.ClientAddress;
+import com.example.administrador.projeto1.model.services.CepService;
 import com.example.administrador.projeto1.util.FormHelper;
 
 public class SaveClientActivity extends AppCompatActivity {
@@ -21,6 +24,8 @@ public class SaveClientActivity extends AppCompatActivity {
     private EditText txtAge;
     private EditText txtAddress;
     private EditText txtPhone;
+    private EditText txtCep;
+    private Button btnFindCep;
     private Client client;
 
     @Override
@@ -28,11 +33,30 @@ public class SaveClientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_save);
 
+        bindFildes();
+        getParameters();
+    }
+
+    private void bindFildes() {
         txtName = (EditText) findViewById(R.id.txtName);
         txtAge = (EditText) findViewById(R.id.txtAge);
         txtAddress = (EditText) findViewById(R.id.txtAddress);
         txtPhone = (EditText) findViewById(R.id.txtPhone);
+        txtCep = (EditText) findViewById(R.id.txtCep);
+        bindButton();
+    }
 
+    private void bindButton() {
+        btnFindCep = (Button) findViewById(R.id.btnFindCep);
+        btnFindCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new GetAddressByCep().execute(txtCep.getText().toString());
+            }
+        });
+    }
+
+    private void getParameters() {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             client = (Client) extras.getParcelable(CLIENT_PARAM);
@@ -63,7 +87,7 @@ public class SaveClientActivity extends AppCompatActivity {
     }
 
     private void bindCliente() {
-        if(client == null){
+        if (client == null) {
             client = new Client();
         }
         client.setName(txtName.getText().toString());
@@ -79,6 +103,25 @@ public class SaveClientActivity extends AppCompatActivity {
         txtAddress.setText(client.getAddress());
         txtPhone.setText(client.getPhone());
     }
+
+    private class GetAddressByCep extends AsyncTask<String, Void, ClientAddress> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected ClientAddress doInBackground(String... params) {
+            return CepService.getAddressBy(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ClientAddress aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
 
 }
 
